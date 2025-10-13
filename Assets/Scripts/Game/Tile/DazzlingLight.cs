e1898d1caf54b20c3a9a018b3c3a95dbc74a1e4e
Nguyen Khac Hieu <20020195@vnu.edu.vn>
2025-10-13 23:02:02 +0700

    feat: update formation apply effect after merge main

--- Assets/Scripts/Game/Tile/DazzlingLight.cs
+++ Assets/Scripts/Game/Tile/DazzlingLight.cs
@@ -27,7 +27,7 @@ namespace Game.Tile
         public override void OnPieceEnter(PieceLogic piece)
         {
             base.OnPieceEnter(piece);
-            ActionManager.ExecuteImmediately(new ApplyEffect(new Blinded(1, 100, piece)));
+            ActionManager.EnqueueAction(new ApplyEffect(new Blinded(1, 100, piece)));
         }
 
         public override void OnPieceExit(PieceLogic piece)
