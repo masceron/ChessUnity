6a7e2bc7652398b1a1d85892fc004bc76c406f04
VaguePasta <qphuc21158@gmail.com>
2025-10-19 15:22:57 +0700

    Merge dung

--- Assets/Scripts/Game/Effects/Others/HammerOysterPassive.cs
+++ Assets/Scripts/Game/Effects/Others/HammerOysterPassive.cs
@@ -1,11 +1,11 @@
-﻿
-using Game.Action;
+﻿using Game.Action;
 using Game.Action.Internal;
 using Game.Managers;
 using Game.Piece.PieceLogic;
 using Game.Relics;
 using static Game.Common.BoardUtils;
 using static Config;
+
 namespace Game.Effects.Others
 {
     public class HammerOysterPassive : Effect, IEndTurnEffect
@@ -16,41 +16,36 @@ namespace Game.Effects.Others
         }
 
         public EndTurnEffectType EndTurnEffectType { get; }
+
         public void OnCallEnd(Action.Action lastMainAction)
         {
             var piecePos = Piece.Pos;
-            if (IsAtPromotionRank(piecePos))
+            if (!IsAtPromotionRank(piecePos)) return;
+            ActionManager.EnqueueAction(new KillPiece(piecePos));
+
+            if (Piece.Color == true)
             {
-                ActionManager.EnqueueAction(new KillPiece(piecePos));
-                
-                if (Piece.Color == true)
-                {
-                    var blackRelic = GetRelicOf(true);
-                    if (blackRelic != null && blackRelic.Type == RelicType.CommonPearl)
-                    {
-                        relicBlackConfig = new RelicConfig(RelicType.BlackPearl, true, 4);
-                        MatchManager.Ins.GameState.BlackRelic = GameState.GetRelicLogicByConfig(relicBlackConfig);
-                        MatchManager.Ins.InputProcessor.UpdateRelic();
-                    }
-                    // var whiteRelic = GetRelicOf(false);
-                    // if (whiteRelic != null && whiteRelic.Type == RelicType.CommonPearl)
-                    // {
-                    //     relicWhiteConfig = new RelicConfig(RelicType.BlackPearl, false, 4);
-                    //     MatchManager.Ins.GameState.WhiteRelic = GameState.GetRelicLogicByConfig(relicWhiteConfig);
-                    //     MatchManager.Ins.InputProcessor.UpdateRelic();
-                    // }
-                }
-                else
-                {
-                    var whiteRelic = GetRelicOf(false);
-                    if (whiteRelic != null && whiteRelic.Type == RelicType.CommonPearl)
-                    {
-                        relicWhiteConfig = new RelicConfig(RelicType.BlackPearl, false, 4);
-                        MatchManager.Ins.GameState.WhiteRelic = GameState.GetRelicLogicByConfig(relicWhiteConfig);
-                        MatchManager.Ins.InputProcessor.UpdateRelic();
-                    }
-                }
+                var blackRelic = GetRelicOf(true);
+                if (blackRelic is not { Type: RelicType.CommonPearl }) return;
+                MatchManager.Ins.GameState.BlackRelic =
+                    GameState.GetRelicLogicByConfig(new RelicConfig(RelicType.BlackPearl, true, 4));
+                // var whiteRelic = GetRelicOf(false);
+                // if (whiteRelic != null && whiteRelic.Type == RelicType.CommonPearl)
+                // {
+                //     relicWhiteConfig = new RelicConfig(RelicType.BlackPearl, false, 4);
+                //     MatchManager.Ins.GameState.WhiteRelic = GameState.GetRelicLogicByConfig(relicWhiteConfig);
+                //     MatchManager.Ins.InputProcessor.UpdateRelic();
+                // }
             }
+            else
+            {
+                var whiteRelic = GetRelicOf(false);
+                if (whiteRelic is not { Type: RelicType.CommonPearl }) return;
+                MatchManager.Ins.GameState.WhiteRelic =
+                    GameState.GetRelicLogicByConfig(new RelicConfig(RelicType.BlackPearl, false, 4));
+            }
+
+            MatchManager.Ins.InputProcessor.UpdateRelic();
         }
     }
 }
