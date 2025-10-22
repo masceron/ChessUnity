ae388c7ff8e425fc1b501f6b8b74341a417d87f3
Dungnn <dungnn@falcongames.com>
2025-10-22 20:23:49 +0700

    BloodMoon regional effect.

---
+++ Assets/Scripts/Game/Effects/RegionalEffect/BloodMoon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Game.Action;
+using Game.Action.Internal;
+using Game.Effects.Debuffs;
+using Game.Managers;
+using Game.Piece.PieceLogic;
+using UnityEngine;
+namespace Game.Effects.RegionalEffect
+{
+    public class BloodMoon : RegionalEffect
+    {
+        protected override void ApplyEffect(int currentTurn)
+        {
+            if (MatchManager.Ins.GameState.IsDay) return;
+            PieceLogic[] board = MatchManager.Ins.GameState.PieceBoard;
+            List<PieceLogic> pieces = new List<PieceLogic>();
+            foreach(PieceLogic piece in board){
+                if (piece != null){
+                    pieces.Add(piece);
+                }
+            }
+            int randomInd = Random.Range(0, pieces.Count);
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Bleeding(pieces[randomInd])));
+        }
+    }
+}
