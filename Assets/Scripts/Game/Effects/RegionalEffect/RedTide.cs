6aadb4d2a6efb0f900e279f912945dbc13bf4fd5
khanhcy <quockhanhcy@gmail.com>
2025-11-01 19:48:04 +0700

    implement RedTide

---
+++ Assets/Scripts/Game/Effects/RegionalEffect/RedTide.cs
@@ -0,0 +1,41 @@
+using Game.Effects.Buffs;
+using Game.Effects.Debuffs;
+using Game.Managers;
+using Game.Piece.PieceLogic;
+using UnityEngine;
+using Game.Action.Internal;
+using Game.Action;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Common;
+using Game.Piece;
+
+namespace Game.Effects.RegionalEffect
+{
+    public class RedTide: RegionalEffect
+    {
+        private int isActive;
+        public RedTide() : base()
+        {
+            isActive = 1;
+        }
+        protected override void ApplyEffect(int currentTurn)
+        {
+            if (isActive == 10)
+            {
+                var random = new System.Random();
+                int randomColumn = random.Next(0, MatchManager.Ins.startingSize.x);
+                for (int i = 0; i < MatchManager.Ins.startingSize.y; i++)
+                {
+                    var piece = BoardUtils.PieceOn(BoardUtils.PosMap(MatchManager.Ins.startingSize.x * i + randomColumn, MatchManager.Ins.startingSize));
+                    if (piece != null) {
+                        ActionManager.EnqueueAction(new ApplyEffect(new Infected(piece)));
+                    }
+                }
+                isActive = 0;
+            }
+            isActive++;
+        }
+
+    }
+}
