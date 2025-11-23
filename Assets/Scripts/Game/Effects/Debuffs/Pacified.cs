826b71cf4907f36edc40567cb3dd911bb888663a
thinhhvt15 <100845976+thinhhvt15@users.noreply.github.com>
2025-11-23 16:04:42 +0700

    fix pacified + sirensHarpoon

--- Assets/Scripts/Game/Effects/Debuffs/Pacified.cs
+++ Assets/Scripts/Game/Effects/Debuffs/Pacified.cs
@@ -1,3 +1,4 @@
+using Game.Piece;
 using Game.Piece.PieceLogic.Commons;
 
 namespace Game.Effects.Debuffs
@@ -5,19 +6,20 @@ namespace Game.Effects.Debuffs
     public class Pacified : Effect
     {
 
+        private byte lastAttackRange;
         public Pacified(PieceLogic piece) : base(-1, 1, piece, "effect_pacified")
         {
-            
+            lastAttackRange = Piece.AttackRange;
         }
         
         public override void OnCallPieceAction(Action.Action action)
         {
-            
+            Piece.AttackRange = 0;
         }
 
         public override void OnRemove()
         {
-            
+            Piece.AttackRange = lastAttackRange;
         }
     }
 }
--- Assets/Scripts/Game/Relics/SirensHarpoon/SirensHarpoonPending.cs
+++ Assets/Scripts/Game/Relics/SirensHarpoon/SirensHarpoonPending.cs
@@ -14,7 +14,6 @@ namespace Game.Relics.SirensHarpoon
 
     public class SirensHarpoonPending : Action.Action, IPendingAble, System.IDisposable
     {
-        public static PieceLogic FirstTarget;
         private SirensHarpoon _sirensHarpoon;
         public SirensHarpoonPending(SirensHarpoon s, int maker, bool pos = false) : base(maker, pos)
         {
@@ -24,37 +23,17 @@ namespace Game.Relics.SirensHarpoon
 
         public void CompleteAction()
         {
-            var hovering = BoardUtils.PieceOn(BoardViewer.HoveringPos);
-
-            if (FirstTarget == null)
-            {
-                FirstTarget = hovering;
-                TileManager.Ins.Select(FirstTarget.Pos);
-                ActionManager.ExecuteImmediately(new ApplyEffect(new Controlled(2, FirstTarget)));
-                TileManager.Ins.UnmarkAll();
-                
-                if (BoardViewer.SelectingFunction == 0)
-                {
-                    BoardViewer.Selecting = -1;
-                    _sirensHarpoon.SetCooldown();
-                    MatchManager.Ins.InputProcessor.UpdateRelic();
-                    ResetTargets();
-                }
-            }
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Controlled(1, BoardUtils.PieceOn(Maker))));
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Pacified(BoardUtils.PieceOn(Maker))));
+            
+            _sirensHarpoon.SetCooldown();
+            MatchManager.Ins.InputProcessor.UpdateRelic();
         }
-
         
-        private static void ResetTargets()
-        {
-            FirstTarget = null;
-        }
 
         public void Dispose()
         {
-            ResetTargets();
-
             _sirensHarpoon = null;
-
             BoardViewer.SelectingFunction = 0;
         }
 
