1e3aec8d5220b993a19e384f57b9fa2cf17bcf13
thinhhvt15 <100845976+thinhhvt15@users.noreply.github.com>
2025-11-30 14:23:09 +0700

    m

--- Assets/Scripts/Game/Action/Internal/Pending/Relic/SirensHarpoonPending.cs
+++ Assets/Scripts/Game/Action/Internal/Pending/Relic/SirensHarpoonPending.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Game.Common;
 using Game.Effects.Debuffs;
 using Game.Managers;
@@ -12,6 +14,7 @@ namespace Game.Action.Internal.Pending.Relic
     public class SirensHarpoonPending : Action, IPendingAble, System.IDisposable
     {
         private SirensHarpoon _sirensHarpoon;
+
         public SirensHarpoonPending(SirensHarpoon s, int maker, bool pos = false) : base(maker, pos)
         {
             _sirensHarpoon = s;
@@ -20,28 +23,54 @@ namespace Game.Action.Internal.Pending.Relic
 
         public void CompleteAction()
         {
-            ActionManager.ExecuteImmediately(new ApplyEffect(new Controlled(1, BoardUtils.PieceOn(Maker))));
-            ActionManager.ExecuteImmediately(new ApplyEffect(new Pacified(BoardUtils.PieceOn(Maker))));
-            
-            _sirensHarpoon.SetCooldown();
-            MatchManager.Ins.InputProcessor.UpdateRelic();
+            ActivateRelic();
         }
-        
+
 
         public void Dispose()
         {
             _sirensHarpoon = null;
             BoardViewer.SelectingFunction = 0;
         }
-        
+
+
+        public void ActivateRelic()
+        {
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Controlled(1, BoardUtils.PieceOn(Maker))));
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Pacified(BoardUtils.PieceOn(Maker))));
+
+            _sirensHarpoon.SetCooldown();
+            MatchManager.Ins.InputProcessor.UpdateRelic();
+        }
+
         protected override void ModifyGameState()
         {
+
         }
 
         public void CompleteActionForAI()
         {
             //Implement for AI automatically
+            var listPieces = new List<PieceLogic>();
+
+            for (var i = 0; i < BoardUtils.BoardSize; ++i)
+            {
+                var piece = BoardUtils.PieceOn(i);
+                if (piece == null || piece.Color == _sirensHarpoon.Color) continue;
+
+                if (piece.Effects.Any(e => (
+                        e.EffectName == "effect_extremophile" || e.EffectName == "effect_sanity"))
+                   ) continue;
+
+                listPieces.Add(piece);
+            }
+
+            if (listPieces.Count == 0) return;
+
+            if (listPieces.Count == 1)
+            {
+
+            }
         }
     }
-
 }
