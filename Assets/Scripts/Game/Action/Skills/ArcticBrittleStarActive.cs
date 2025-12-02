79794bb3800d5cc60fa1dacbe16b577aba6ce15d
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2025-12-02 09:46:40 +0700

    feat: add ArcticBrittleStarActive AI

--- Assets/Scripts/Game/Action/Skills/ArcticBrittleStarActive.cs
+++ Assets/Scripts/Game/Action/Skills/ArcticBrittleStarActive.cs
@@ -4,6 +4,9 @@ using Game.Piece.PieceLogic.Commons;
 using Game.Tile;
 using UnityEngine;
 using Game.AI;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Common;
 
 namespace Game.Action.Skills
 {
@@ -27,7 +30,30 @@ namespace Game.Action.Skills
 
         public void CompleteActionForAI()
         {
-            throw new System.NotImplementedException();
+            var listPieces = new List<PieceLogic>();
+            
+            foreach (var (rank, file) in MoveEnumerators.AroundUntil(RankOf(Maker), FileOf(Maker), 3))
+            {
+                var idx = IndexOf(rank, file);
+                var pOn = PieceOn(idx);
+                if (pOn != null && pOn.Color != PieceOn(Maker).Color)
+                {
+                    if(pOn.Effects != null && pOn.Effects.Any(e => e.EffectName == "effect_extremophile")) continue;
+                    listPieces.Add(pOn);
+                }
+            }
+            if (listPieces.Count == 0) return;
+            int maxValue = listPieces.Max(p => p.GetValueForAI());
+            var bestPieces = listPieces.Where(p => p.GetValueForAI() == maxValue).ToList();
+            if (bestPieces.Count == 0) return;
+
+            var random = new System.Random();
+            var selectedPiece = bestPieces[random.Next(bestPieces.Count)];
+            Formation AnchorIce = new AnchorIce(PieceOn(Maker).Color);
+            AnchorIce.SetDuration(3);
+            FormationManager.Ins.SetFormation(selectedPiece.Pos, AnchorIce);
+
+            SetCooldown(Maker, ((IPieceWithSkill)PieceOn(Maker)).TimeToCooldown);
         }
     }
 }
