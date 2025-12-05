64ba274a5871e33131b69dabf1521bc723da16e6
thinhhvt15 <100845976+thinhhvt15@users.noreply.github.com>
2025-12-05 15:37:18 +0700

    Archerfish Active AI

--- Assets/Scripts/Game/Action/Skills/ArcherfishActive.cs
+++ Assets/Scripts/Game/Action/Skills/ArcherfishActive.cs
@@ -1,8 +1,10 @@
-﻿using static Game.Common.BoardUtils;
+﻿using System.Linq;
+using static Game.Common.BoardUtils;
 using Game.Action.Internal;
 using Game.Effects.Debuffs;
 using Game.Piece.PieceLogic.Commons;
 using Game.AI;
+using Unity.Android.Gradle.Manifest;
 
 namespace Game.Action.Skills
 {
@@ -31,7 +33,20 @@ namespace Game.Action.Skills
 
         public void CompleteActionForAI()
         {
-            throw new System.NotImplementedException();
+            var (rank, file) = RankFileOf(Maker);
+            var listPieces = GetPiecesInRadius(rank, file,4, p => p != null && p.Color != PieceOn(Maker).Color)
+                .Where(p => p.Effects.Any(p => p.EffectName != "effect_extremophile"
+                                          && p.EffectName != "effect_blind" &&
+                                          p.EffectName != "effect_marked")).ToList();
+            
+            if (listPieces.Count == 0) return;
+            listPieces.Sort((a, b) => a.GetValueForAI().CompareTo(b.GetValueForAI()));
+            
+            var idx = UnityEngine.Random.Range(0, listPieces.Count);
+            ActionManager.EnqueueAction(new ApplyEffect(new Blinded(2, 100, listPieces[idx])));
+            ActionManager.EnqueueAction(new ApplyEffect(new Marked(2, listPieces[idx])));
+            
+            SetCooldown(Maker, ((IPieceWithSkill)PieceOn(Maker)).TimeToCooldown);
         }
 
     }
