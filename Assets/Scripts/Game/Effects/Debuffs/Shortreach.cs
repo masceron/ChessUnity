caec0589677c221e79e65489ad7662dcb2e40ff3
Dungnn <nguyenngocdung19122005@gmail.com>
2025-12-16 15:31:56 +0700

    Implement logics for Shortreach debuff.

--- Assets/Scripts/Game/Effects/Debuffs/Shortreach.cs
+++ Assets/Scripts/Game/Effects/Debuffs/Shortreach.cs
@@ -6,8 +6,24 @@ namespace Game.Effects.Debuffs
     {
         public Shortreach(sbyte duration, sbyte strength, PieceLogic piece) : base(duration, strength, piece, "effect_shortreach")
         {
+            if (Strength > 0)
+            {
+                Piece.AttackRange -= (byte)Strength;
+                if (Piece.AttackRange < 1)
+                {
+                    Piece.AttackRange = 1;
+                }
+            }
         }
-        
+
+        public override void OnRemove()
+        {
+            if (Strength > 0)
+            {
+                Piece.AttackRange += (byte)Strength;
+            }
+        }
+
         public override int GetValueForAI()
         {
             return base.GetValueForAI() - (Strength * 15 + Duration * 5);
