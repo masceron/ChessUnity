d536f5b1186cf4e6f6aae5e505ad8abe3b67fe31
Nguyễn Thanh Bình <binhbocnc1234@gmail.com>
2025-12-19 12:01:18 +0700

    Tối ưu UI cho regional

--- Assets/Scripts/UX/UI/FreePlayTest/RegionalRealmScene/RegionalSearcher.cs
+++ Assets/Scripts/UX/UI/FreePlayTest/RegionalRealmScene/RegionalSearcher.cs
@@ -15,7 +15,9 @@ namespace UX.UI.FreePlayTest.RegionalRealmScene
         private List<RegionalEffectType> lastSearchResult;
         public void Load()
         {
+            lastSearchResult = new List<RegionalEffectType>() { RegionalEffectType.None };
             lastSearchResult = Enum.GetValues(typeof(RegionalEffectType)).Cast<RegionalEffectType>().ToList();
+            lastSearchResult.RemoveAt(lastSearchResult.Count - 1);
             DisplaySearchResult();
         }
         private void DisplaySearchResult()
