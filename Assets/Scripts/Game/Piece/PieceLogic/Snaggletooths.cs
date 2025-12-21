51c35eb2389b2233ce0aeb7dba4c8f9393b48507
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2025-12-21 08:07:55 +0700

    feat: add Snaggletooths AI

--- Assets/Scripts/Game/Piece/PieceLogic/Snaggletooths.cs
+++ Assets/Scripts/Game/Piece/PieceLogic/Snaggletooths.cs
@@ -4,6 +4,8 @@ using Game.Common;
 using Game.Movesets;
 using Game.Piece.PieceLogic.Commons;
 using static Game.Common.BoardUtils;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Game.Piece.PieceLogic
 {
@@ -37,7 +39,29 @@ namespace Game.Piece.PieceLogic
                 }
                 else
                 {
+                    var listPieces = new List<Commons.PieceLogic>();
                     //query for AI in here
+                    if (!excludeEmptyTile)
+                    {
+                        var (rank, file) = RankFileOf(Pos);
+                        foreach (var (rankOff, fileOff) in MoveEnumerators.AroundUntil(rank, file, 2))
+                        {
+                            var index = IndexOf(rankOff, fileOff);
+                            var piece = PieceOn(index);
+                            if (piece == null) continue;
+                            if (piece.Effects.Any(e => e.EffectName == "effect_bleeding"))
+                            {
+                                list.Add(new SnaggletoothsActive(Pos, index, false));
+                                listPieces.Add(piece);
+                            }
+                        }
+                    }
+                    if (listPieces.Count > 0)
+                    {
+                        int randomIndex = Random.Range(0, listPieces.Count);
+                        list.Add(new SnaggletoothsActive(Pos, listPieces[randomIndex].Pos, true));
+                    }
+                    
                 }
             };
         }
