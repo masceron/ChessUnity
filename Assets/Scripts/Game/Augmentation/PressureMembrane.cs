59db1a032f18ebd79b8bb57767b2f1816b0c6d25
Dungnn <nguyenngocdung19122005@gmail.com>
2026-01-08 16:19:40 +0700

    Add PressureMembrane passive effect to augmentation logic

--- Assets/Scripts/Game/Augmentation/PressureMembrane.cs
+++ Assets/Scripts/Game/Augmentation/PressureMembrane.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Game.Augmentation.Set;
 using Game.Effects;
+using Game.Effects.Augmentation;
 using Game.Piece.PieceLogic.Commons;
 
 namespace Game.Augmentation
@@ -16,7 +17,7 @@ namespace Game.Augmentation
         {
             Target = target;
             Set = new AugmentationSet(AugmentationSetType.None,false);
-            
+            PassiveEffects.Add(new PressureMembranePassive(Target));
         }
     }
 }
