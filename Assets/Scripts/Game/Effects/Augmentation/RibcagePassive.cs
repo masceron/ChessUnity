8f51b715d553070d554283506ad7c4c2aee06290
Nguyễn Thanh Bình <binhbocnc1234@gmail.com>
2026-01-10 17:18:31 +0700

    Update RibcagePassive.cs

--- Assets/Scripts/Game/Effects/Augmentation/RibcagePassive.cs
+++ Assets/Scripts/Game/Effects/Augmentation/RibcagePassive.cs
@@ -1,20 +1,32 @@
+using System.Linq;
 using Game.Action;
 using Game.Action.Internal;
+using Game.Common;
 using Game.Effects.Traits;
 using Game.Piece.PieceLogic.Commons;
+using static Game.Common.BoardUtils;
 
 namespace Game.Effects.Augmentation
 {
-    public class RibcagePassive : Effect, IAfterPieceActionEffect
+    public class RibcagePassive : Effect, IStartTurnEffect
     {
         public RibcagePassive(sbyte duration, sbyte strength, PieceLogic piece) : base(duration, strength, piece,
             "effect_ribcage_passive")
-        { }
-
-        public void OnCallAfterPieceAction(Action.Action action)
         {
+            StartTurnEffectType = StartTurnEffectType.StartOfAllyTurn;
+        }
+        public StartTurnEffectType StartTurnEffectType { get; }
+        public void OnCallStart(Action.Action lastMainAction)
+        {
+            var (rank, file) = RankFileOf(Piece.Pos);
             
-            ActionManager.EnqueueAction(new ApplyEffect(new LongReach(Piece, 1, -1)));
+            foreach (var (r, f) in MoveEnumerators.AroundUntil(rank, file, 4))
+            {
+                var pOn = PieceOn(IndexOf(r, f));
+                if (pOn == null || pOn.Color != Piece.Color) continue; 
+                ActionManager.EnqueueAction(new ApplyEffect(new LongReach(Piece, 1, 1)));
+            }
+           
         }
     }
 }
