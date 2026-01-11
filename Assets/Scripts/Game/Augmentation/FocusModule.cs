c01ad697b4ac28ac08651f856215a821b13b1b3b
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-01-11 08:38:20 +0700

    feat: add augmentation focusModule

---
+++ Assets/Scripts/Game/Augmentation/FocusModule.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Game.Augmentation.Set;
+using Game.Effects;
+using Game.Effects.Augmentation;
+using Game.Piece.PieceLogic.Commons;
+
+namespace Game.Augmentation
+{
+    [Il2CppSetOption(Option.NullChecks, false), Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    public class FocusModule : Augmentation
+    {
+        public FocusModule() : base(AugmentationName.FocusModule, AugmentationRarity.Rare, AugmentationSlot.Fin, null, null)
+        {
+            PassiveEffects = new List<Effect>();
+        }
+        
+        public override void SetTarget(PieceLogic target)
+        {
+
+            Target = target;
+            Set = new AugmentationSet(AugmentationSetType.None,false);
+            PassiveEffects.Add(new FocusModulePassive(Target));
+        }
+    }
+}
