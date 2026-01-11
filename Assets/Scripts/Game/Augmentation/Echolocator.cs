a23864a422448e879ab7ed28940e603deebca2b3
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-01-11 08:37:31 +0700

    feat: add augmentation echolocator

---
+++ Assets/Scripts/Game/Augmentation/Echolocator.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Game.Augmentation.Set;
+using Game.Effects;
+using Game.Effects.Augmentation;
+using Game.Piece.PieceLogic.Commons;
+
+namespace Game.Augmentation
+{
+    [Il2CppSetOption(Option.NullChecks, false), Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    public class Echolocator : Augmentation
+    {
+        public Echolocator() : base(AugmentationName.Echolocator, AugmentationRarity.Rare, AugmentationSlot.Fin, null, null)
+        {
+            PassiveEffects = new List<Effect>();
+        }
+        
+        public override void SetTarget(PieceLogic target)
+        {
+
+            Target = target;
+            Set = new AugmentationSet(AugmentationSetType.None,false);
+            PassiveEffects.Add(new EcholocatorPassive(Target));
+        }
+    }
+}
