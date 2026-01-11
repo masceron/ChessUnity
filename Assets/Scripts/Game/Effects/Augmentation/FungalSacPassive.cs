741e054a9c0657dc847c491966e1a89d43ded37c
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-01-11 08:40:16 +0700

    feat: add augmentation stressRegulator

---
+++ Assets/Scripts/Game/Effects/Augmentation/FungalSacPassive.cs
@@ -0,0 +1,24 @@
+    using Game.Action;
+using Game.Action.Internal;
+using Game.Piece.PieceLogic.Commons;
+using Game.Effects.Buffs;
+using Game.Effects.Debuffs;
+namespace Game.Effects.Augmentation
+{
+    public class FungalSacPassive : Effect, IApplyEffect
+    {
+        private const int turnCounter = 2;
+        private const int radius = 4;
+        public FungalSacPassive(PieceLogic piece) : base(-1, 1, piece, "effect_fungal_sac_passive")
+        {
+        }
+
+        public void OnCallApplyEffect(ApplyEffect applyEffect)
+        {
+            var infected = new Infected(Piece);
+            infected.SetTurnCounter(turnCounter);
+            infected.SetRadius(radius);
+            ActionManager.EnqueueAction(new ApplyEffect(infected));
+        }
+    }
+}
