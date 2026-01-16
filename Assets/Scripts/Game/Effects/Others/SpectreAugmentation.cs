333659424847c1375b906fdbb00b208c3349cd8c
NgDung <nguyenngocdung19122005@gmail.com>
2026-01-16 14:58:50 +0700

    Add SpectreAugmentation effect with associated logic, assets, and integration.

---
+++ Assets/Scripts/Game/Effects/Others/SpectreAugmentation.cs
@@ -0,0 +1,22 @@
+﻿
+using Game.Action.Internal;
+using Game.Piece.PieceLogic.Commons;
+
+namespace Game.Effects.Others
+{
+    public class SpectreAugmentation : Effect, IApplyEffect
+    {
+        public SpectreAugmentation(PieceLogic piece) : base(-1, 1, piece, "effect_spectre_augmentation")
+        {
+        }
+        
+        public void OnCallApplyEffect(ApplyEffect applyEffect)
+        {
+            if (applyEffect.Effect.Category == EffectCategory.Debuff && applyEffect.SourcePiece == Piece)
+            {
+                // TODO: Get random buff effect when this piece owns spectre augmentation.
+                // Just one buff effect for each turn. 
+            }
+        }
+    }
+}
