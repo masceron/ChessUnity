c2b0e1dd987d8f07525db46ff450beb2ddb3c89a
NgDung <nguyenngocdung19122005@gmail.com>
2026-01-16 14:40:31 +0700

    Update ShadowFin and TaintedHeart to use Spectre AugmentationSet; add Spectre and ScrapCollector types to AugmentationSet logic.

--- Assets/Scripts/Game/Augmentation/TaintedHeart.cs
+++ Assets/Scripts/Game/Augmentation/TaintedHeart.cs
@@ -16,8 +16,7 @@ namespace Game.Augmentation
         public override void SetTarget(PieceLogic target)
         {
             Target = target;
-            Set = new AugmentationSet(AugmentationSetType.None,false);
-            //TODO: Change AugmentationSetType to Spectre.
+            Set = new AugmentationSet(AugmentationSetType.Spectre,true);
             PassiveEffects.Add(new TaintedHeartPassive(Target));
         }
     }
