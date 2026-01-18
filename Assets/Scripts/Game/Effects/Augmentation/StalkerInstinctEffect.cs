df3287bf86f58f7cf73d0fa0eb3a50e3d16c0e72
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-01-18 20:32:27 +0700

    feat: add set StalkerInstinct

---
+++ Assets/Scripts/Game/Effects/Augmentation/StalkerInstinctEffect.cs
@@ -0,0 +1,35 @@
+using Game.Action;
+using Game.Action.Internal;
+using Game.Piece.PieceLogic.Commons;
+using Game.Effects.Buffs;
+using Game.Effects.Debuffs;
+using System.Collections.Generic;
+using static Game.Common.BoardUtils;
+using Game.Action.Captures;
+using System.Linq;
+namespace Game.Effects.Augmentation
+{
+    public class StalkerInstinctEffect : Effect, IOnMoveGenEffect
+    {
+        public StalkerInstinctEffect(PieceLogic piece) : base(-1, 1, piece, "effect_stalker_instinct")
+        {
+        }
+
+        public void OnCallMoveGen(PieceLogic caller, List<Action.Action> actions)
+        {
+            if (caller != Piece) return;
+             var markedEnemies = FindPiecesWithEffectName(!caller.Color, "effect_marked");
+             foreach (var enemy in markedEnemies)
+            {
+                if (enemy != null && IsAlive(enemy))
+                {
+                     bool alreadyExists = actions.Any(a => a is ICaptures && a.Target == enemy.Pos);
+                     if (!alreadyExists)
+                     {
+                        actions.Add(new NormalCapture(caller.Pos, enemy.Pos));
+                     }
+                }
+            }
+        }
+    }
+}
