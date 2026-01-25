4f8c93e472e8ee90bca9f4f88ff4d7f767460784
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-01-25 08:10:40 +0700

    fix:  BottlenoseDolphinPassive

--- Assets/Scripts/Game/Effects/Condition/BottlenoseDolphinPassive.cs
+++ Assets/Scripts/Game/Effects/Condition/BottlenoseDolphinPassive.cs
@@ -4,67 +4,67 @@ using Game.Action;
 using Game.Effects.Traits;
 using System.Linq;
 using Game.Piece.PieceLogic.Commons;
-
+using System;
 namespace Game.Effects.Condition
 {
     [Il2CppSetOption(Option.NullChecks, false), Il2CppSetOption(Option.ArrayBoundsChecks, false)]
-    public class BottlenoseDolphinPassive: Effect, IEndTurnEffect
+    public class BottlenoseDolphinPassive: Effect, IStartTurnEffect
     {
-        private bool surpassed, evaded;
-
+        private bool surpassed;
+        private int evasionProbability = 25;
         public BottlenoseDolphinPassive(PieceLogic piece) : base(-1, 1, piece, "effect_bottlenose_dolphin_passive")
         {
-            EndTurnEffectType = EndTurnEffectType.EndOfAllyTurn;
-            surpassed = !MatchManager.Ins.GameState.IsDay && Piece.Effects.All(e => e.EffectName != "effect_surpass");
-            evaded = !MatchManager.Ins.GameState.IsDay && Piece.Effects.All(e => e.EffectName != "effect_evasion");
-            if (!surpassed)
-            {
-                ActionManager.EnqueueAction(new ApplyEffect(new Surpass(Piece)));
-                surpassed = true;
-            }
-            if (!evaded)
-            {
-                ActionManager.EnqueueAction(new ApplyEffect(new Evasion(-1, 25, Piece)));
-                evaded = true;
-            }
+            StartTurnEffectType = StartTurnEffectType.StartOfAllyTurn;
         }
 
-        public EndTurnEffectType EndTurnEffectType { get; }
+        public StartTurnEffectType StartTurnEffectType { get; }
 
-        public void OnCallEnd(Action.Action lastMainAction)
+        public void OnCallStart(Action.Action lastMainAction)
         {
+            surpassed = Piece.Effects.Any(e => e.EffectName == "effect_surpass");
             if (MatchManager.Ins.GameState.IsDay && !surpassed) 
             {
-                surpassed = true;
                 ActionManager.EnqueueAction(new ApplyEffect(new Surpass(Piece)));
             }
-            if (MatchManager.Ins.GameState.IsDay && !evaded)
+            if (MatchManager.Ins.GameState.IsDay)
             {
-                evaded = true;
-                ActionManager.EnqueueAction(new ApplyEffect(new Evasion(-1, 25, Piece)));
+                var existingEvasion = Piece.Effects.OfType<Evasion>().FirstOrDefault();
+                if (existingEvasion != null)
+                {
+                    existingEvasion.Probability = Math.Max(existingEvasion.Probability, evasionProbability);
+                }
+                else
+                {
+                    ActionManager.EnqueueAction(new ApplyEffect(new Evasion(-1, evasionProbability, Piece)));
+                }
             }
 
             if (!MatchManager.Ins.GameState.IsDay)
             {
-                var surpassEffect = Piece.Effects.Find(e => e.EffectName == "effect_surpass");
+                var surpassEffect = Piece.Effects.OfType<Surpass>().FirstOrDefault();
                 if (surpassEffect != null)
+                {
                     ActionManager.EnqueueAction(new RemoveEffect(surpassEffect));
-                    
-                var evasionEffect = Piece.Effects.Find(e => e.EffectName == "effect_evasion");
+                }
+                var evasionEffect = Piece.Effects.OfType<Evasion>().FirstOrDefault();
                 if (evasionEffect != null)
-                    ActionManager.EnqueueAction(new RemoveEffect(evasionEffect));
-                    
-                surpassed = false;
-                evaded = false;
+                {
+                    if (evasionEffect.Probability <= evasionProbability)
+                    {
+                        ActionManager.EnqueueAction(new RemoveEffect(evasionEffect));
+                    }
+                    else
+                    {
+                        evasionEffect.Probability -= evasionProbability;
+                    }
+                }
+                
             }
-
-
         }
 
         public override int GetValueForAI()
         {
             return base.GetValueForAI() - 20;
         }
-
     }
 }
