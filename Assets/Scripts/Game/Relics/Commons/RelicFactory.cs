ce0579688afca4e552e639f332694ea43dce3535
PQP <51254101+masceron@users.noreply.github.com>
2026-01-25 19:54:26 +0700

    Merged thinh

--- Assets/Scripts/Game/Relics/Commons/RelicFactory.cs
+++ Assets/Scripts/Game/Relics/Commons/RelicFactory.cs
@@ -8,12 +8,14 @@ namespace Game.Relics.Commons
             {
                 "relic_adrenaline_radiator" => new AdrenalineRadiator(cfg),
                 "relic_black_pearl" => new BlackPearl(cfg),
+                "relic_chaotic_constructor" => new ChaoticConstructor(cfg),
                 "relic_common_pearl" => new CommonPearl(cfg),
                 "relic_coral_tome" => new CoralTome(cfg),
                 "relic_eye_of_mimic" => new EyeOfMimic(cfg),
                 "relic_frost_sigil" => new FrostSigil(cfg),
                 "relic_hermos_horn" => new HermosHorn(cfg),
                 "relic_kelp_banner" => new KelpBanner(cfg),
+                "relic_ledger_stone" => new LedgerStone(cfg),
                 "relic_mangrove_charm" => new MangroveCharm(cfg),
                 "relic_methane_casing" => new MethaneCasing(cfg),
                 "relic_ornetes_edict" => new OrnetesEdict(cfg),
@@ -23,10 +25,11 @@ namespace Game.Relics.Commons
                 "relic_ray_stinger" => new RayStinger(cfg),
                 "relic_reliquary" => new Reliquary(cfg),
                 "relic_rotting_scythe" => new RottingScythe(cfg),
+                "relic_seabed_leveler" => new SeabedLeveler(cfg),
                 "relic_seafoam_phial" => new SeafoamPhial(cfg),
                 "relic_sirens_harpoon" => new SirensHarpoon(cfg),
                 "relic_storm_capacitor" => new StormCapacitor(cfg),
-                "relic_ledger_stone" => new LedgerStone(cfg),
+                "relic_temporal_warp" => new TemporalWarp(cfg),
                 _ => null
             };
         }
