b74453223d28327ab3a87fee05d3047cc94fe680
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-02-01 16:00:22 +0700

    fix: relic ledgerStone

--- Assets/Scripts/Game/Action/Internal/Pending/Relic/LedgerStonePending.cs
+++ Assets/Scripts/Game/Action/Internal/Pending/Relic/LedgerStonePending.cs
@@ -1,4 +1,4 @@
-    using System.Linq;
+using System.Linq;
 using Game.Common;
 using Game.Effects;
 using Game.Managers;
@@ -9,167 +9,40 @@ using UX.UI.Ingame;
 using Game.Action.Relics;
 using static Game.Common.BoardUtils;
 using Game.Piece;
+using Game.Action.Internal.Pending;
+using Game.Action.Internal;
+
 namespace Game.Action.Internal.Pending.Relic
 {
     [Il2CppSetOption(Option.NullChecks, false), Il2CppSetOption(Option.ArrayBoundsChecks, false)]
-    public class LedgerStonePending : Action, System.IDisposable, IRelicAction
+    public class LedgerStonePending : PendingAction, System.IDisposable, IRelicAction, IInternal
     {
         private LedgerStone ledgerStone;
         private bool isFirstOption;
+        
         public LedgerStonePending(LedgerStone cp, bool isFirstOption) : base(-1)
         {
             this.ledgerStone = cp;
             this.isFirstOption = isFirstOption;
         }
 
-        public void Dispose()
+        public override void CompleteAction()
         {
-            ledgerStone = null;
-        }
-
-        protected override void ModifyGameState()
-        {
-            UnityEngine.Debug.Log("LedgerStonePending: " + isFirstOption);
-            int startingSizeY = (MaxLength - MatchManager.Ins.StartingSize.y) / 2;
-            int startingSizeX = (MaxLength - MatchManager.Ins.StartingSize.x) / 2;
-            if (isFirstOption)
+           
+            
+            var excute = new LedgerStoneExcute(isFirstOption);
+            BoardViewer.Ins.ExecuteAction(excute);
+            if(ledgerStone != null)
             {
-                bool isFillColumn = false;
-                if(!IsColumnFull(startingSizeY)){
-                    isFillColumn = true;
-                }
-                if(isFillColumn)
-                {
-                    for (var rank = startingSizeX; rank < startingSizeX + MatchManager.Ins.StartingSize.x; rank++)
-                    {
-                        var index = IndexOf(rank, startingSizeY);
-                        if (TileManager.Ins.IsTileEmpty(index))
-                        {
-                            TileManager.Ins.ActivateTile(rank, startingSizeY);
-                        }
-                    }
-                } else {
-                    var leftColumnFile = startingSizeY - 1;
-                    if (leftColumnFile >= 0)
-                    {
-                        for (var rank = startingSizeX; rank < startingSizeX + MatchManager.Ins.StartingSize.x; rank++)
-                        {
-                            TileManager.Ins.ActivateTile(rank, leftColumnFile);
-                        }
-                    }
-                }
-
-                isFillColumn = false;
-                var RightColumnFile = (MaxLength - MatchManager.Ins.StartingSize.y) / 2 + MatchManager.Ins.StartingSize.x - 1;
-                if(!IsColumnFull(RightColumnFile)){
-                    isFillColumn = true;
-                }
-                if(isFillColumn)
-                {
-                    for (var rank = startingSizeX; rank < startingSizeX + MatchManager.Ins.StartingSize.x; rank++)
-                    {
-                        var index = IndexOf(rank, RightColumnFile);
-                        if (TileManager.Ins.IsTileEmpty(index))
-                        {
-                            TileManager.Ins.ActivateTile(rank, RightColumnFile);
-                        }
-                    }
-                } else {
-                    RightColumnFile++;
-                    if (RightColumnFile >= 0)
-                    {
-                        for (var rank = startingSizeX; rank < startingSizeX + MatchManager.Ins.StartingSize.x; rank++)
-                        {
-                            TileManager.Ins.ActivateTile(rank, RightColumnFile);
-                        }
-                    }
-                }
-
-
+                ledgerStone.SetCooldown();
             }
-            else
-            {
-                bool isFillRow = false;
-                if(!IsRowFull(startingSizeX)){
-                    isFillRow = true;
-                }
-                if(isFillRow)
-                {
-                    for (var file = startingSizeY; file < startingSizeY + MatchManager.Ins.StartingSize.y; file++)
-                    {
-                        var index = IndexOf(startingSizeX, file);
-                        if (TileManager.Ins.IsTileEmpty(index))
-                        {
-                            TileManager.Ins.ActivateTile(startingSizeX, file);
-                        }
-                    }
-                } else {
-                    var topRowRank = startingSizeX - 1;
-                    if (topRowRank >= 0)
-                    {
-                        for (var file = startingSizeY; file < startingSizeY + MatchManager.Ins.StartingSize.y; file++)
-                        {
-                            TileManager.Ins.ActivateTile(topRowRank, file);
-                        }
-                    }
-                }
-
-                isFillRow = false;
-                var bottomRowRank = startingSizeX + MatchManager.Ins.StartingSize.x - 1;
-                if(!IsRowFull(bottomRowRank)){
-                    isFillRow = true;
-                }
-                if(isFillRow)
-                {
-                    for (var file = startingSizeY; file < startingSizeY + MatchManager.Ins.StartingSize.y; file++)
-                    {
-                        var index = IndexOf(bottomRowRank, file);
-                        if (TileManager.Ins.IsTileEmpty(index))
-                        {
-                            TileManager.Ins.ActivateTile(bottomRowRank, file);
-                        }
-                    }
-                } else {
-                    var bottomRowRankPlus = bottomRowRank + 1;
-                    if (bottomRowRankPlus < MaxLength)
-                    {
-                        for (var file = startingSizeY; file < startingSizeY + MatchManager.Ins.StartingSize.y; file++)
-                        {
-                            TileManager.Ins.ActivateTile(bottomRowRankPlus, file);
-                        }
-                    }
-                }
-            }
-            ledgerStone.SetCooldown();
             MatchManager.Ins.InputProcessor.UpdateRelic();
             Dispose();
         }
 
-        private bool IsColumnFull(int column)
+        public void Dispose()
         {
-            var startingSizeX = (MaxLength - MatchManager.Ins.StartingSize.x) / 2;
-            for (var i = startingSizeX; i < startingSizeX + MatchManager.Ins.StartingSize.x; i++)
-            {
-                UnityEngine.Debug.Log("IsColumnFull: " + i + ", " + column + ", " + MatchManager.Ins.StartingSize.x);
-                if(TileManager.Ins.IsTileEmpty(IndexOf(i,column))) 
-                {
-                    UnityEngine.Debug.Log("IsNotColumnFull");
-                    return false;
-                }
-            }
-            return true;
-        }
-        private bool IsRowFull(int row)
-        {
-            var startingSizeY = (MaxLength - MatchManager.Ins.StartingSize.y) / 2;
-            for (var file = startingSizeY; file < startingSizeY + MatchManager.Ins.StartingSize.y; file++)
-            {
-                if(TileManager.Ins.IsTileEmpty(IndexOf(row, file))) 
-                {
-                    return false;
-                }
-            }
-            return true;
+            ledgerStone = null;
         }
     }
 }
