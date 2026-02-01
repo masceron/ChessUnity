5a044df11973f17e958c400d8d04be03500c417c
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-02-01 15:58:53 +0700

    fix: relic commonpearl

---
+++ Assets/Scripts/Game/Action/Relics/CommonPearlExcute.cs
@@ -0,0 +1,65 @@
+using Game.Action.Internal;
+using Game.Common;
+using Game.Effects;
+using Game.Effects.Debuffs;
+using Game.Managers;
+using Game.Piece.PieceLogic.Commons;
+using UnityEngine;
+using Game.Effects.Buffs;
+using System.Linq;
+
+namespace Game.Action.Relics
+{
+
+    public class CommonPearlExcute : Action, IRelicAction
+    {
+        private int Target;
+
+        public CommonPearlExcute(int Target) : base(Target)
+        {
+            this.Target = Target;
+        }
+
+        protected override void ModifyGameState()
+        {
+            var effect = GetRandomBuffEffect(BoardUtils.PieceOn(Target));
+            if (effect == null)
+            {
+                return; 
+            }
+            ActionManager.EnqueueAction(new ApplyEffect(effect));
+        }
+
+        public Effect GetRandomBuffEffect(PieceLogic piece)
+        {
+            var buffEffects = AssetManager.Ins.EffectData
+                .Where(kvp => kvp.Value.category == EffectCategory.Buff)
+                .Select(kvp => kvp.Key)
+                .ToArray();
+            
+            var random = new System.Random();
+            var selectedEffectName = buffEffects[random.Next(buffEffects.Length)];
+            UnityEngine.Debug.Log("Selected Effect Name: " + selectedEffectName);
+            return CreateEffectFromName("effect_shield", piece);
+        }
+
+        private static Effect CreateEffectFromName(string effectName, PieceLogic piece)
+        {
+            var randomDuration = (sbyte)new System.Random().Next(2, 6);
+            
+            // TODO: Add more effects missing
+            return effectName switch
+            {
+                "effect_shield" => new Effects.Buffs.Shield(piece),
+                "effect_carapace" => new Effects.Buffs.Carapace(randomDuration, piece),
+                "effect_haste" => new Effects.Buffs.Haste(randomDuration, 1, piece),
+                "effect_piercing" => new Effects.Buffs.Piercing(randomDuration, piece),
+                "effect_hardened_shield" => new Effects.Buffs.HardenedShield(piece),
+                "effect_true_bite" => new Effects.Buffs.TrueBite(piece),
+                "effect_camouflage" => new Effects.Buffs.Camouflage(piece),
+
+                _ => null
+            };
+        }
+    }
+}
