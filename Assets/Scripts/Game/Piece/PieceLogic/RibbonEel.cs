4a1afa4c73a3a9e450c054091ce68ec3074c693a
NgDung <nguyenngocdung19122005@gmail.com>
2026-02-04 22:37:27 +0700

    Implement RibbonEel piece with associated logic, assets, and data updates.

---
+++ Assets/Scripts/Game/Piece/PieceLogic/RibbonEel.cs
@@ -0,0 +1,16 @@
+﻿using Game.Action;
+using Game.Action.Internal;
+using Game.Effects.Buffs;
+using Game.Movesets;
+
+namespace Game.Piece.PieceLogic
+{
+    public class RibbonEel : Commons.PieceLogic
+    {
+        public RibbonEel(PieceConfig cfg) : base(cfg, AmbushPredatorMoves.Quiets, KingMoves.Captures)
+        {
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Camouflage(this)));
+            ActionManager.ExecuteImmediately(new ApplyEffect(new TrueBite(this)));
+        }
+    }
+}
