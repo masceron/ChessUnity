318f20c12c371ad4e6369f442b11784291637c4c
Nguyễn Thanh Bình <binhbocnc1234@gmail.com>
2026-02-11 00:56:28 +0700

    Update thêm file bị sót cho PaletailUnicornfish

---
+++ Assets/Scripts/Game/Piece/PieceLogic/PaletailUnicornfish.cs
@@ -0,0 +1,23 @@
+using Game.Action;
+using Game.Action.Internal;
+using Game.Action.Internal.Pending.Piece;
+using Game.Action.Skills;
+using Game.Effects.SpecialAbility;
+using Game.Effects.Traits;
+using Game.Movesets;
+using Game.Piece.PieceLogic.Commons;
+using Game.Tile;
+using UnityEngine;
+using static Game.Common.BoardUtils;
+
+namespace Game.Piece.PieceLogic
+{
+    public class PaletailUnicornfish : Commons.PieceLogic
+    {
+        public PaletailUnicornfish(PieceConfig cfg) : base(cfg, BishopMoves.Quiets, BishopMoves.Captures)
+        {
+            ActionManager.ExecuteImmediately(new ApplyEffect(new PaletailUnicornfishPassive(this)));
+            
+        }
+    }
+}
