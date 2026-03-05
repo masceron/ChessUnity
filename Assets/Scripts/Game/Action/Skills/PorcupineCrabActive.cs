49b167cbea9418ea8e35c1dbac5d8cb821be09e3
Dungnn <nguyenngocdung19122005@gmail.com>
2026-03-05 14:58:32 +0700

    Add PorcupineCrab skill implementation.

--- Assets/Scripts/Game/Action/Skills/PorcupineCrabActive.cs
+++ Assets/Scripts/Game/Action/Skills/PorcupineCrabActive.cs
@@ -1,10 +1,18 @@
-﻿using Game.Piece.PieceLogic.Commons;
+﻿using System;
+using System.Collections.Generic;
+using Game.Action.Internal;
+using Game.Action.Quiets;
+using Game.Effects.Debuffs;
+using Game.Piece.PieceLogic.Commons;
+using static Game.Common.BoardUtils;
 using MemoryPack;
+using UnityEngine.Rendering;
 
 namespace Game.Action.Skills
 {
     public class PorcupineCrabActive : Action, ISkills
     {
+        private const int BleedingStack = 5;
         [MemoryPackConstructor]
         private PorcupineCrabActive()
         {
@@ -18,7 +26,58 @@ namespace Game.Action.Skills
         
         protected override void ModifyGameState()
         {
-            throw new System.NotImplementedException();
+            ActionManager.EnqueueAction(new NormalMove(Maker, Target));
+            var (rank1, file1) = RankFileOf(Maker);
+            var (rank2, file2) = RankFileOf(Target);
+            
+            var path = GetPath(rank1, file1, rank2, file2);
+            
+            foreach (var (rank, file) in path)
+            {   
+                var pOn = PieceOn(IndexOf(rank, file));
+                if (pOn != null && pOn.Color != PieceOn(Maker).Color)
+                {
+                    ActionManager.EnqueueAction(new ApplyEffect(new Bleeding(BleedingStack, pOn), PieceOn(Maker)));
+                }
+            }
+        }
+        
+        List<(int rank, int file)> GetPath(int rank1, int file1, int rank2, int file2)    
+        {
+            var path = new List<(int, int)>();
+
+            int dRank = rank2 - rank1;
+            int dFile = file2 - file1;
+
+            int steps = GCD(Math.Abs(dRank), Math.Abs(dFile));
+
+            if (steps == 0) return path; // cùng 1 điểm
+
+            int stepRank = dRank / steps;
+            int stepFile = dFile / steps;
+
+            int currentRank = rank1 + stepRank;
+            int currentFile = file1 + stepFile;
+
+            for (int i = 1; i < steps; i++)
+            {
+                path.Add((currentRank, currentFile));
+                currentRank += stepRank;
+                currentFile += stepFile;
+            }
+
+            return path;
+        }
+
+        int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
         }
 
         public int AIPenaltyValue(PieceLogic maker)
--- Assets/Scripts/Game/Piece/PieceLogic/PorcupineCrab.cs
+++ Assets/Scripts/Game/Piece/PieceLogic/PorcupineCrab.cs
@@ -13,10 +13,15 @@ namespace Game.Piece.PieceLogic
     public class PorcupineCrab : Commons.PieceLogic, IPieceWithSkill
     {
         private int timeToCooldown;
+        private const int Cooldown = 6;
+        private const int Range = 3;
 
         public PorcupineCrab(PieceConfig cfg) : base(cfg, ShellfishMoves.Quiets, BishopMoves.Captures)
         {
             ActionManager.ExecuteImmediately(new ApplyEffect(new Carapace(-1,this)));
+            
+            SetStat(SkillStat.Cooldown, Cooldown);
+            SetStat(SkillStat.Range, 3);
             
             Skills = (list, isPlayer, excludeEmptyTile) =>
             {
@@ -25,7 +30,7 @@ namespace Game.Piece.PieceLogic
                 {
                     var (rank, file) = RankFileOf(Pos);
                     
-                    foreach (var (rankOff, fileOff) in MoveEnumerators.AroundUntil(rank, file, 2))
+                    foreach (var (rankOff, fileOff) in MoveEnumerators.AroundUntil(rank, file, GetStat(SkillStat.Range)))
                     {
                         var index = IndexOf(rankOff, fileOff);
                         var pOn = PieceOn(index);
