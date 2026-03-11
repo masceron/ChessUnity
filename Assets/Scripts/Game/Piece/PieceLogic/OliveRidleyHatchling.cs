d0f1ff080cff5884ea13fc4c6c8d42fbcaed2fa2
thinhhvt15 <100845976+thinhhvt15@users.noreply.github.com>
2026-03-11 15:34:38 +0700

    Create OliveRidleyHatchling.cs

---
+++ Assets/Scripts/Game/Piece/PieceLogic/OliveRidleyHatchling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Piece.PieceLogic
+{
+    public class OliveRidleyHatchling : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
