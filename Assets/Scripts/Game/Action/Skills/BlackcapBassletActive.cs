e3bbb618b200e86126c2f02b5d14a40c4647ca28
Dungnn <nguyenngocdung19122005@gmail.com>
2026-03-16 13:59:52 +0700

    Update BlackcapBasslet skill logic and implement active skill functionality.

--- Assets/Scripts/Game/Action/Skills/BlackcapBassletActive.cs
+++ Assets/Scripts/Game/Action/Skills/BlackcapBassletActive.cs
@@ -1,7 +1,24 @@
-﻿namespace Game.Action.Skills
+﻿using Game.Action.Quiets;
+using Game.Piece.PieceLogic.Commons;
+
+namespace Game.Action.Skills
 {
-    public class BlackcapBassletActive
+    public class BlackcapBassletActive : Action, ISkills
     {
+        public BlackcapBassletActive(int maker, int target) : base(maker)
+        {
+            Maker = maker;
+            Target = target;
+        }
         
+        protected override void ModifyGameState()
+        {
+            ActionManager.EnqueueAction(new NormalMove(Maker, Target));
+        }
+
+        public int AIPenaltyValue(PieceLogic maker)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
