d8144dc91b14445cf7c495950ecf4effe78cf1cc
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-03-22 00:36:41 +0700

    feat: add EmptyDiamondMove

---
+++ Assets/Scripts/Game/Movesets/EmptyDiamondMove.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using static Game.Common.BoardUtils;
+
+namespace Game.Movesets
+{
+    public class EmptyDiamondMove : BaseMovePattern
+    {
+        public override List<int> GenerateBaseMovePattern(int makerPos)
+        {
+            return GenerateEmptyDiamondMovePattern(makerPos);
+        }
+
+        private List<int> GenerateEmptyDiamondMovePattern(int makerPos)
+        {
+            var caller = PieceOn(makerPos);
+            var positions = new List<int>();
+            var push = caller.Color ? +1 : -1;
+            var (rank, file) = RankFileOf(makerPos);
+
+            positions.Add(IndexOf(rank + push * 2, file));
+            positions.Add(IndexOf(rank - push * 2, file));
+
+            positions.Add(IndexOf(rank + push * 1, file + 1));
+            positions.Add(IndexOf(rank + push * 1, file - 1));
+            positions.Add(IndexOf(rank - push * 1, file + 1));
+            positions.Add(IndexOf(rank - push * 1, file - 1));
+
+            positions.Add(IndexOf(rank, file + 2));
+            positions.Add(IndexOf(rank, file - 2));
+
+            return positions;
+        }
+
+        public static int Quiets(List<Action.Action> list, int pos, bool excludeEmptyTile)
+        {
+            var moveRange = PieceOn(pos).GetMoveRange();
+            var basePattern = new HashSet<int>(new EmptyDiamondMove().GenerateEmptyDiamondMovePattern(pos));
+            AddToPatternMoves(list, basePattern, pos, moveRange, false, excludeEmptyTile);
+
+            return 30 + 5 * moveRange;
+        }
+
+        public static int Captures(List<Action.Action> list, int pos, bool excludeEmptyTile)
+        {
+            var attackRange = PieceOn(pos).GetAttackRange();
+            var basePattern = new HashSet<int>(new EmptyDiamondMove().GenerateEmptyDiamondMovePattern(pos));
+            AddToPatternMoves(list, basePattern, pos, attackRange, true, excludeEmptyTile);
+            return 30 + 5 * attackRange;
+        }
+    }
+}
