17830176ac91bf4fdeecaa962864337af5d0e311
Dungnn <nguyenngocdung19122005@gmail.com>
2026-04-07 22:31:02 +0700

    Refactor GulperEelActive skill to use CooldownSkill action and adjust effect application logic.

--- Assets/Scripts/Game/Piece/PieceLogic/GulperEel.cs
+++ Assets/Scripts/Game/Piece/PieceLogic/GulperEel.cs
@@ -1,4 +1,7 @@
+using Game.Action;
+using Game.Action.Internal;
 using Game.Action.Skills;
+using Game.Effects.Traits;
 using Game.Movesets;
 using Game.Piece.PieceLogic.Commons;
 using static Game.Common.BoardUtils;
@@ -36,7 +39,7 @@ namespace Game.Piece.PieceLogic
                 //query for AI in here
             };
 
-            //ActionManager.ExecuteImmediately(new ApplyEffect(new Surpass(this)));
+            ActionManager.ExecuteImmediately(new ApplyEffect(new Surpass(this)));
         }
 
         public SkillsDelegate Skills { get; set; }
