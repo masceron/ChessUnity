2299ac13314638d51d022d37501ae2f5704fd95a
Dungnn <nguyenngocdung19122005@gmail.com>
2026-04-07 19:14:22 +0700

    Refactor EpauletteShark skill to use CooldownSkill action instead of setting cooldown directly.

--- Assets/Scripts/Game/Action/Skills/EpauletteSharkActive.cs
+++ Assets/Scripts/Game/Action/Skills/EpauletteSharkActive.cs
@@ -26,7 +26,7 @@ namespace Game.Action.Skills
         protected override void ModifyGameState()
         {
             ActionManager.EnqueueAction(new KillPiece(GetMaker(), GetTargetAsPiece()));
-            SetCooldown(GetMakerAsPiece(), ((IPieceWithSkill)GetMakerAsPiece()).TimeToCooldown);
+            ActionManager.EnqueueAction(new CooldownSkill(GetMakerAsPiece()));
         }
     }
 }
