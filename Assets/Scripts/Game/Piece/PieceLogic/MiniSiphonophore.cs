783a21a642787306e3d5810559ad178cfe483977
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-04-07 11:56:27 +0700

    fix: MiniSiphonophore
    fix stat

--- Assets/Scripts/Game/Piece/PieceLogic/MiniSiphonophore.cs
+++ Assets/Scripts/Game/Piece/PieceLogic/MiniSiphonophore.cs
@@ -9,15 +9,17 @@ namespace Game.Piece.PieceLogic
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     public class MiniSiphonophore : Commons.PieceLogic, IPieceWithSkill
     {
-        public MiniSiphonophore(PieceConfig cfg) : base(cfg, RangerMove.Quiets, RangerMove.Captures)
+        public MiniSiphonophore(PieceConfig cfg) : base(cfg, KingMoves.Quiets, KingMoves.Captures)
         {
+            SetStat(SkillStat.Range, 1);
+            SetStat(SkillStat.Unit, 1);
 
             Skills = (list, isPlayer, excludeEmptyTile) =>
             {
                 if (SkillCooldown > 0) return;
                 if (isPlayer)
                 {
-                    var targets = SkillRangeHelper.GetActiveEnemyPieceInRadius(this, 2);
+                    var targets = SkillRangeHelper.GetActiveEnemyPieceInRadius(this, GetStat(SkillStat.Range));
                     foreach (var target in targets) list.Add(new MiniSiphonophoreActive(this, target));
                 }
             };
